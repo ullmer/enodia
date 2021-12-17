075679f10919fdacdf9b7025099688a2e50838ca
Brygg Ullmer <bullmer@clemson.edu>
2021-12-17 15:10:56 -0500

    stubs

---
+++ src/entanglerUi.02/ei-teibk.pl
@@ -0,0 +1,10 @@
+% Enodia interaction panel : geographical : world 01
+% Brygg Ullmer, Clemson University
+% Begun 2021-12-17
+
+module(ei-geo-world01, []).
+
+:- use_module(library(yaml)).
+:- use_module(library(dicts)).
+
+%%% end %%%
