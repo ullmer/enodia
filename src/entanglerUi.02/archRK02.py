3f3e0d8e70506f7806ce36c83111845ca84dd681
ullmer <ullmer@acm.org>
2021-12-18 01:19:57 -0500

    abstracting

---
+++ src/entanglerUi.02/archRK02.py
@@ -0,0 +1,29 @@
+# Wikipedia Architecture parsing 
+# By Brygg Ullmer, Clemson University
+# Begun 2021-12-17
+
+import wikipediaapi
+import wptools
+
+%%%%%%%%%%%%%%%%% get buildings %%%%%%%%%%%%%%%%%
+
+def getBldgs(targetpage):
+  ww=wikipediaapi.Wikipedia('en')
+  page1 = ww.page(targetpage)
+  buildings = page1.categorymembers.keys()
+  return buildings
+
+%%%%%%%%%%%%%%%%% get building location %%%%%%%%%%%%%%%%%
+
+def getBldgLocation(targetpage):
+  page = wptools.page(targetpage).get_parse()
+  infobox = page2.data['infobox']
+  location = infobox['location']
+  return location
+
+rkb   = "Category:Rem Koolhaas buildings"
+bldgs = getBldgs(rkb)
+
+#  page = wptools.page('Seattle Central Library').get_parse()
+
+### end ###
