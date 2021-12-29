05c42820963adbcd21b905a4858cec2fa0860b7a
Brygg Ullmer <ullmer@acm.org>
2021-12-29 00:17:48 -0500

    debugging

--- elements/panels/usa.sc.parks/wikipediaSCparks01.py
+++ elements/panels/usa.sc.parks/wikipediaSCparks01.py
@@ -5,13 +5,27 @@
 import wikipediaapi
 import wptools
 
+
+#parks, site, area
+
 ##################### get buildings #####################
 
 def getCategoryMembers(targetpage):
   ww=wikipediaapi.Wikipedia('en')
   page1 = ww.page(targetpage)
-  buildings = page1.categorymembers.keys()
-  return buildings
+  categories = page1.categorymembers.keys()
+  return categories
+
+def getLinks(targetpage):
+  ww=wikipediaapi.Wikipedia('en')
+  page1 = ww.page(targetpage)
+  links = page1.links()
+  return links
+
+################## get building location ##################
+
+#def getBldgLocation(targetpage):
+#  page = wptools.page(targetpage, silent=True).get_parse()
 
 ################## get building location ##################
 
@@ -24,13 +38,14 @@ def getBldgLocation(targetpage):
   except: return None
 
 scParks = "List of South Carolina state parks"
-parkEls = getCategoryMembers(rkb)
-print(parkEls)
+#parkEls = getCategoryMembers(scParks)
+linkHash  = getLinks(scParks)
 
 idx = 0
-for park in parkEls:
+for link in linkHash.keys():
   idx += 1
   if idx > 7: break
+  print(linkHash[link])
 
   #loc = getBldgLocation(b)
   #print('Building %s : location %s' % (b, loc))
