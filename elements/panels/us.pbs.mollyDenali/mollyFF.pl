a27baf629508d967f78e9c4300a35ee2daa4848f
Brygg Ullmer <brygg@Bryggs-Mac-mini.local>
2022-01-01 17:41:37 -0500

    progress

--- elements/panels/us.pbs.mollyDenali/mollyFF.pl
+++ elements/panels/us.pbs.mollyDenali/mollyFF.pl
@@ -8,8 +8,9 @@
 :- discontiguous(parents/3).
 
 girls([molly.mabray, trini.mumford, vera.malakas])
+boys([tookone.ookami, oscar.marsh]).
 
-friends[molly.mabray, tookone.ookami, trini.mumford]).
+friends(molly.mabray, [tookone.ookami, trini.mumford, oscar.marsh]).
 
 dogs([suki]).
 
@@ -19,11 +20,16 @@ pets(tookone.ookami, [anka, luka, laika, tukoni, skippy, jax,
 
 parents(layla.mabray, walter-mabray, [molly.mabray]). married(layla.mabray, walter.mabray).
 parents(atsaq.ookami, kenji.ookami,  [tookone.ookami, jay.ookami, john.ookami]).
+parents(joy.mumford, daniel.mumford, [trini.mumford]).
+parents(renate.marsh, omf,           [oscar.marsh]).
+parents(midge.marsh,  rmf,           [renate.marsh]).
+parents(mmm, mmf,                    [midge.marsh, annie.marsh]).
 parents(nkw, nehtan.kon, [layla.mabray]).
 
 person(molly.mabray, mabra, molly).
 
 nicknames(tookkone.ookami, [tooey]).
+nicknames(midge.marsh, [auntie.midge]).
 nicknames(nehtan.kon, [nat, grandpa.nat, lightning]).
 
 %nicknames(molly.mabray, [molly]).
