30e5c2a5e7ae721911449c8197b58af94445f478
ullmer <ullmer@acm.org>
2022-01-02 16:19:56 -0500

    progress

--- elements/panels/us.pbs.mollyDenali/mollyFF.pl
+++ elements/panels/us.pbs.mollyDenali/mollyFF.pl
@@ -1,6 +1,5 @@
 % Family example: Molly of Denali family and friends
 % Begun 2022-01-01 by Brygg Ullmer, Clemson University
-% https://en.wikipedia.org/wiki/Roosevelt_family
 
 %:- use_module(family).
 
@@ -26,19 +25,23 @@ parents(midge_marsh,  rmf,           [renate_marsh]).
 parents(mmm, mmf,                    [midge_marsh, annie_marsh]).
 parents(nkw, nehtan_kon, [layla_mabray]).
 
-personMeta(flf, [full, last, first]). %specify level of detail to describe people
+personMeta(flfn, [full, last, first, nickname]). %specify level of detail to describe people
+personMeta(flf,  [full, last, first]).           %specify level of detail to describe people
 
 person(flf, [molly_mabray,  mabray, molly]).
 person(flf, [layla_mabray,  mabray, layla]).
 person(flf, [walter_mabray, mabray, walter]).
-person(flf, [trini_mumford, mumford, trini]).
-person(flf, [tookone_ookami, ookami, tookkone]).
-person(flf, [atsaq_ookami,   ookami, atsaq]).
-person(flf, [kenji_ookami,   ookami, kenji]).
 
-nicknames(tookkone_ookami, [tooey]).
-nicknames(midge_marsh, [auntie_midge]).
-nicknames(nehtan_kon, [nat, grandpa_nat, lightning]).
+person(flf, [trini_mumford,  mumford, trini]).
+person(flf, [joy_mumford,    mumford, joy]
+person(flf, [daniel_mumford, mumford, daniel]).
+
+person(flfn, [tookone_ookami, ookami, tookkone, [tooey]]).
+person(flf,  [atsaq_ookami,   ookami, atsaq]).
+person(flf,  [kenji_ookami,   ookami, kenji]).
+
+person(flfn, [midge_marsh, marsh, midge,  [auntie_midge, chief_midge]).
+person(flfn, [nehtan_kon,  kon,   nehtan, [nat, grandpa_nat, lightning]).
 
 %:s/.*/\L&/
 
