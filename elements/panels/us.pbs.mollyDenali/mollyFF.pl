d9a78b782166237999e20139fdd9757bc3d30e46
ullmer <ullmer@acm.org>
2022-01-02 16:15:05 -0500

    progress

--- elements/panels/us.pbs.mollyDenali/mollyFF.pl
+++ elements/panels/us.pbs.mollyDenali/mollyFF.pl
@@ -8,9 +8,9 @@
 :- discontiguous(parents/3).
 
 girls([molly_mabray, trini_mumford, vera_malakas]).
-boys([tookone_ookami, oscar_marsh]).
+boys([tookkone_ookami, oscar_marsh]).
 
-friends(molly_mabray, [tookone_ookami, trini_mumford, oscar_marsh]).
+friends(molly_mabray, [tookkone_ookami, trini_mumford, oscar_marsh]).
 
 dogs([suki]).
 
@@ -19,16 +19,22 @@ pets(tookone_ookami, [anka, luka, laika, tukoni, skippy, jax,
                       rascal, kiwi, kobi, atsoo, sasha, mouse, bandifer]).
 
 parents(layla_mabray, walter_mabray, [molly_mabray]).  married(layla_mabray, walter_mabray).
-parents(atsaq_ookami, kenji_ookami,  [tookone_ookami, jay_ookami, john_ookami]).
+parents(atsaq_ookami, kenji_ookami,  [tookkone_ookami, jay_ookami, john_ookami]).
 parents(joy_mumford, daniel_mumford, [trini_mumford]). married(joy_mumford, daniel_mumford).
 parents(renate_marsh, omf,           [oscar_marsh]).   
 parents(midge_marsh,  rmf,           [renate_marsh]).
 parents(mmm, mmf,                    [midge_marsh, annie_marsh]).
 parents(nkw, nehtan_kon, [layla_mabray]).
 
-person(molly_mabray, mabray, molly).
-person(layla_mabray, mabray, layla).
-person(
+personMeta(flf, [full, last, first]). %specify level of detail to describe people
+
+person(flf, [molly_mabray,  mabray, molly]).
+person(flf, [layla_mabray,  mabray, layla]).
+person(flf, [walter_mabray, mabray, walter]).
+person(flf, [trini_mumford, mumford, trini]).
+person(flf, [tookone_ookami, ookami, tookkone]).
+person(flf, [atsaq_ookami,   ookami, atsaq]).
+person(flf, [kenji_ookami,   ookami, kenji]).
 
 nicknames(tookkone_ookami, [tooey]).
 nicknames(midge_marsh, [auntie_midge]).
