88c6e972dc08960c5ff8fcd920d93b76f19614c4
Brygg Ullmer <brygg@Bryggs-Mac-mini.local>
2022-01-07 16:23:29 -0500

    LKU

--- elements/panels/us.pbs.mollyDenali/mollyFF.pl
+++ elements/panels/us.pbs.mollyDenali/mollyFF.pl
@@ -17,31 +17,31 @@ pets(molly,   [suki, bandifer]).
 pets(tooey, [anka, luka, laika, tukoni, skippy, jax, 
                       rascal, kiwi, kobi, atsoo, sasha, mouse, bandifer]).
 
-parents(layla, walter, [molly]).  married(layla, walter).
-parents(atsaq, kenji,  [tooey, jay, john]).
-parents(joy, daniel,   [trini]). married(joy, daniel).
+parents(layla, walter, [molly]).            married(layla, walter).
+parents(atsaq, kenji,  [tooey, jay, john]). married(atsaq, kenji).
+parents(joy, daniel,   [trini]).            married(joy, daniel).
 parents(renate, omf,   [oscar]).   
 parents(midge,  rmf,           [renate]).
 parents(mmm, mmf,                    [midge_marsh, annie_marsh]).
-parents(nkw, nehtan_kon, [layla_mabray]).
+parents(nkw, nat, [layla_mabray]).
 
 personMeta(flfn, [full, last, first, nickname]). %specify level of detail to describe people
 personMeta(flf,  [full, last, first]).           %specify level of detail to describe people
 
-person(lf, molly_mabray,  [mabray, molly]).
-person(lf, layla_mabray,  [mabray, layla]).
-person(lf, walter_mabray, [mabray, walter]).
+person(lf, molly,  [mabray, molly]).
+person(lf, layla,  [mabray, layla]).
+person(lf, walter, [mabray, walter]).
 
-person(lf, trini_mumford,  [mumford, trini]).
-person(lf, joy_mumford,    [mumford, joy]).
-person(lf, daniel_mumford, [mumford, daniel]).
+person(lf, trini,  [mumford, trini]).
+person(lf, joy,    [mumford, joy]).
+person(lf, daniel, [mumford, daniel]).
 
-person(lfn, tookone_ookami, [ookami, tookkone, [tooey]]).
-person(lf,  atsaq_ookami,   [ookami, atsaq]).
-person(lf,  kenji_ookami,   [ookami, kenji]).
+person(lfn, tooey, [ookami, tookkone, [tooey]]).
+person(lf,  atsaq, [ookami, atsaq]).
+person(lf,  kenji, [ookami, kenji]).
 
-person(lfn, midge_marsh, [marsh, midge,  [auntie_midge, chief_midge]]).
-person(lfn, nehtan_kon,  [kon,   nehtan, [nat, grandpa_nat, lightning]]).
+person(lfn, midge, [marsh, midge,  [auntie_midge, chief_midge]]).
+person(lfn, nat,   [kon,   nehtan, [nat, grandpa_nat, lightning]]).
 
 %:s/.*/\L&/
 
