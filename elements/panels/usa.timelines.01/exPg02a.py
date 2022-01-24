2d773cdc3b83b3f32f182ecea50efced59438ca6
ullmer <ullmer@acm.org>
2022-01-23 23:17:28 -0500

    display two sprites

--- elements/panels/usa.timelines.01/exPg02a.py
+++ elements/panels/usa.timelines.01/exPg02a.py
@@ -1,45 +1,17 @@
-#https://github.com/lordmauve/pgzero/blob/master/examples/basic/demo3.py
+#PyGame Zero warmup on timelines
+#Brygg Ullmer, Clemson University
+#Begun 2022-01-23
 
-alien = Actor('alien')
+t1, t2 = Actor('1860a'), Actor('1880a')
 
-TITLE = "Alien walk"
-WIDTH = 500
-HEIGHT = alien.height + 20
+TITLE = "Timeline ex02a"
+WIDTH = 1920; HEIGHT = 1080
 
-
-# The initial position of the alien
-alien.topright = 0, 10
-
+t1.topleft= 0,   10
+t2.topleft= 600, 10
 
 def draw():
-    """Clear the screen and draw the alien."""
-    screen.clear()
-    alien.draw()
+  screen.clear()
+  t1.draw(); t2.draw()
 
-
-def update():
-    """Move the alien by one pixel."""
-    alien.x += 1
-
-    # If the alien is off the right hand side of the screen,
-    # move it back off screen to the left-hand side
-    if alien.left > WIDTH:
-        alien.right = 0
-
-
-def on_mouse_down(pos):
-    """Detect clicks on the alien."""
-    if alien.collidepoint(pos):
-        set_alien_hurt()
-
-
-def set_alien_hurt():
-    """Set the current alien sprite to the "hurt" image."""
-    alien.image = 'alien_hurt'
-    sounds.eep.play()
-    clock.schedule_unique(set_alien_normal, 1.0)
-
-
-def set_alien_normal():
-    """Set the current alien sprite to the normal image."""
-    alien.image = 'alien'
+### end ###
