b60b4c1a44d428d251fbb31d8d0314739a56add9
ullmer <ullmer@acm.org>
2022-01-24 00:24:27 -0500

    abstraction progress

---
+++ elements/panels/usa.timelines.01/exPg02f.py
@@ -0,0 +1,20 @@
+#PyGame Zero warmup on timelines
+#Brygg Ullmer, Clemson University
+#Begun 2022-01-24
+
+from pgzTimeline import *
+
+pgzt = pgzTimeline()
+
+TITLE = "Timeline ex02f"
+WIDTH = 1920; HEIGHT = 1080
+
+def on_mouse_down(pos):
+  global pgzt; pgzt.on_mouse_down(pos)
+
+def draw():
+  global pgzt
+  screen.clear()
+  pgzt.draw()
+
+### end ###
