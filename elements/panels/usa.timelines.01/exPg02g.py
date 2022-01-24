bbbb159f8d997b691e9a33e9bbf75c6abe32c5a2
ullmer <ullmer@acm.org>
2022-01-24 00:51:51 -0500

    good progress, including touch-to-drag

--- elements/panels/usa.timelines.01/exPg02g.py
+++ elements/panels/usa.timelines.01/exPg02g.py
@@ -10,7 +10,6 @@ TITLE = "Timeline ex02f"
 WIDTH = 1920; HEIGHT = 1080
 
 def on_mouse_down(pos):      global pgzt; pgzt.on_mouse_down(pos)
-def on_mouse_down(pos):      global pgzt; pgzt.on_mouse_down(pos)
 def on_mouse_move(pos, rel): global pgzt; pgzt.on_mouse_move(pos, rel)
 def on_mouse_up(pos):        global pgzt; pgzt.on_mouse_up()
 def draw():                  global pgzt; screen.clear(); pgzt.draw()
