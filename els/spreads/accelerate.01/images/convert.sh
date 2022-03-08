88a2d0dd1ef5fcd73bae43439a3114aeb9b941da
ullmer <ullmer@acm.org>
2022-03-08 00:19:58 -0500

    still confused why majority of time, no output is yielded.

--- els/spreads/accelerate.01/images/convert.sh
+++ els/spreads/accelerate.01/images/convert.sh
@@ -1,11 +1,11 @@
-for n in `seq -w 20`
+for n in `seq -w 50`
 do 
   echo $n
   #mkdir bb$n
   for i in `seq 9`
   do
     echo $i
-    convert bb$n.png -alpha set -background none -channel A \
-         -evaluate multiply 0.$i +channel bb$n/$i.png
+    echo convert bb$n.png -alpha set -background none -channel A \
+         -evaluate set 0.$i +channel bb$n/$i.png
   done
 done
