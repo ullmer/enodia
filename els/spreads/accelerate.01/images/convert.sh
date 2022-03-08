d42a92fa0dac1932d578735e209b8a36381a5d07
ullmer <ullmer@acm.org>
2022-03-07 23:43:30 -0500

    second convert

--- els/spreads/accelerate.01/images/convert.sh
+++ els/spreads/accelerate.01/images/convert.sh
@@ -1,12 +1,11 @@
 for n in `seq -w 20`
 do 
   echo $n
-  mkdir bb$n
-  mv bb$n.jpg bb$n
-  cd bb$n
+  #mkdir bb$n
   for i in `seq 9`
   do
     echo $i
-    convert bb$n.png -alpha set -background none -channel A -evaluate multiply 0.$i +channel $i.png
+    convert bb$n.png -alpha set -background none -channel A \
+         -evaluate multiply 0.$i +channel bb$n/$i.png
   done
 done
