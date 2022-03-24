cbcd1fcd33bd940a70a24160b070753e72126443
ullmer <ullmer@acm.org>
2022-03-24 10:16:05 -0400

    enoPapPanCrop01a.py
    returning to 1a approach; good progress

--- els/spreads/accelerate.01/enoPapPanCrop01b.py
+++ els/spreads/accelerate.01/enoPapPanCrop01b.py
@@ -15,7 +15,8 @@ divsHoriz = 6
 divsVert  = 3
 
 #trimsTBLR = [200, 200, 200, 200]
-trimsTBLR = [200, 1910, 200, 200] #upper band
+#trimsTBLR = [200, 1910, 200, 200] #upper band
+trimsTBLR = [300, 1910, 200, 200] #upper band
 
 srcPdf  = PdfFileReader(open(srcFn, "rb"))
 srcPage = srcPdf.getPage(0)
@@ -34,19 +35,19 @@ spreadHeight = ur[1]-lr[1]
 paneWidth  = int(spreadWidth  / divsHoriz)
 paneHeight = int(spreadHeight / divsVert)
 
-nul = (int(ul[0]+trimsTBLR[2]), int(ul[1]+trimsTBLR[0]))
-nll = (int(ll[0]+trimsTBLR[2]), int(ll[1]+trimsTBLR[1]))
+cropUL = (int(ul[0]+trimsTBLR[2]), int(ul[1]+trimsTBLR[0]))
+cropLR = (int(lr[0]-trimsTBLR[3]), int(lr[1]-trimsTBLR[1]))
 
-#nur = (int(ur[0]-trimsTBLR[3]), int(ur[1]+trimsTBLR[0]))
+#nul = (int(ul[0]+trimsTBLR[2]), int(ul[1]-trimsTBLR[0]))
+#nll = (int(ll[0]+trimsTBLR[2]), int(ll[1]+trimsTBLR[1]))
+#nur = (int(ur[0]-trimsTBLR[3]), int(ur[1]-trimsTBLR[0]))
 #nlr = (int(lr[0]-trimsTBLR[3]), int(lr[1]+trimsTBLR[1]))
-nur = (int(nll[0]+paneWidth), int(ur[1]+trimsTBLR[0]))
-nlr = (int(nll[0]+paneWidth), int(lr[1]+trimsTBLR[1]))
-print(nul, nur, nll, nlr)
+#nur = (int(nll[0]+paneWidth), int(ur[1]-trimsTBLR[0]))
+#nlr = (int(nll[0]+paneWidth), int(lr[1]+trimsTBLR[1]))
+#print(nul, nur, nll, nlr)
 
-srcPage.mediaBox.lowerRight = nlr
-srcPage.mediaBox.lowerLeft  = nll
-srcPage.mediaBox.upperRight = nur
-srcPage.mediaBox.upperLeft  = nul
+srcPage.cropBox.upperLeft  = cropUL
+srcPage.cropBox.lowerRight = cropLR
 
 targPdf.addPage(srcPage)
 targF = open(targFn, 'wb')
