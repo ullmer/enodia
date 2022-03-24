e7afdf54b91821930563453edcacec8496c9e940
Brygg Ullmer <bullmer@clemson.edu>
2022-03-24 10:44:08 -0400

    iterating

--- els/spreads/accelerate.01/enoPapPanCrop01c.py
+++ els/spreads/accelerate.01/enoPapPanCrop01c.py
@@ -16,7 +16,10 @@ srcYaml   = yaml.safe_load(srcYamlF)
 try:    es = srcYaml["enoSpread"]
 except: print("YAML spread error; aborting"); sys.exit(-1)
 
-try:    srcFn = es["srcFn"]; targFn = es["targFn"]
+try:    
+  srcFn    = es["srcFn"]
+  targUbFn = es["targUbFn"] #upper band
+  targLbFn = es["targLbFn"] #lower band
 except: print("YAML FN error; aborting"); sys.exit(-1)
 
 try:    divsHoriz = es["divsHoriz"]; divsVert=es["divsVert"]
@@ -30,13 +33,12 @@ except: print("YAML band error; aborting"); sys.exit(-1)
 srcPdf  = PdfFileReader(open(srcFn, "rb"))
 srcPage = srcPdf.getPage(0)
 
-targPdf = PdfFileWriter()
-
 ll = srcPage.cropBox.getLowerLeft()
 lr = srcPage.cropBox.getLowerRight()
 ul = srcPage.cropBox.getUpperLeft()
 ur = srcPage.cropBox.getUpperRight()
 print(ll, lr, ul, ur)
+srcBounds = [ll, lr, ul, ur]
 
 spreadWidth  = lr[0]-ul[0]
 spreadHeight = ur[1]-lr[1]
@@ -44,24 +46,38 @@ spreadHeight = ur[1]-lr[1]
 paneWidth  = int(spreadWidth  / divsHoriz)
 paneHeight = int(spreadHeight / divsVert)
 
-nul = (int(ul[0]+trimsTBLR[2]), int(ul[1]-trimsTBLR[0]))
-nll = (int(ll[0]+trimsTBLR[2]), int(ll[1]+trimsTBLR[1]))
+########### process band ########### 
 
-nur = (int(ur[0]-trimsTBLR[3]), int(ur[1]-trimsTBLR[0]))
-nlr = (int(lr[0]-trimsTBLR[3]), int(lr[1]+trimsTBLR[1]))
-#nur = (int(nll[0]+paneWidth), int(ur[1]-trimsTBLR[0]))
-#nlr = (int(nll[0]+paneWidth), int(lr[1]+trimsTBLR[1]))
-print(nul, nur, nll, nlr)
+procBand(targFn, trimsTBLR, srcPage, srcBounds):
 
-srcPage.mediaBox.lowerRight = nlr
-srcPage.mediaBox.lowerLeft  = nll
-srcPage.mediaBox.upperRight = nur
-srcPage.mediaBox.upperLeft  = nul
+  ll, lr, ul, ur = srcBounds
 
-targPdf.addPage(srcPage)
-targF = open(targFn, 'wb')
+  nul = (int(ul[0]+trimsTBLR[2]), int(ul[1]-trimsTBLR[0]))
+  nll = (int(ll[0]+trimsTBLR[2]), int(ll[1]+trimsTBLR[1]))
 
-targPdf.write(targF)
-targF.close()
+  nur = (int(ur[0]-trimsTBLR[3]), int(ur[1]-trimsTBLR[0]))
+  nlr = (int(lr[0]-trimsTBLR[3]), int(lr[1]+trimsTBLR[1]))
+  #nur = (int(nll[0]+paneWidth), int(ur[1]-trimsTBLR[0]))
+  #nlr = (int(nll[0]+paneWidth), int(lr[1]+trimsTBLR[1]))
+
+  print(nul, nur, nll, nlr)
+
+  srcPage.mediaBox.upperLeft  = nul
+  srcPage.mediaBox.lowerLeft  = nll
+  srcPage.mediaBox.lowerRight = nlr
+  srcPage.mediaBox.upperRight = nur
+
+  targPdf = PdfFileWriter()
+  targPdf.addPage(srcPage)
+
+  targF = open(targFn, 'wb')
+
+  targPdf.write(targF)
+  targF.close()
+
+########### main ########### 
+
+procBand(targUbFn, trimsUpperBand, srcPage, srcBounds)
+procBand(targLbFn, trimsLowerBand, srcPage, srcBounds)
 
 ### end ###
