4587df7a75156a3d51d752f264bec89a038cacf1
Brygg Ullmer <bullmer@clemson.edu>
2022-04-01 12:34:26 -0400

    iterating

--- els/spreads/accelerate.01/enoSpreads.py
+++ els/spreads/accelerate.01/enoSpreads.py
@@ -2,4 +2,13 @@
 # Brygg Ullmer, Clemson University
 # Begun 2022-04-01
 
+####################### Enodia Spreads ####################### 
+
+class enoSpreads:
+  spreadsYFn = "yaml/spreads.yaml"
+
+####################### Enodia Spread ####################### 
+
+class enoSpread:
+
 ### end ###
