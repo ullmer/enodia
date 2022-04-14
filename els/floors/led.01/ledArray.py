befa4f68c1f46cc5a02344ca94fe418337dead9e
Brygg Ullmer <ullmer@acm.org>
2022-04-13 22:45:31 -0400

    first steps

--- els/floors/led.01/ledArray.py
+++ els/floors/led.01/ledArray.py
@@ -3,13 +3,29 @@
 # Begun 2022-04-13
 
 try: #https://github.com/v923z/micropython-ulab
-    from ulab import numpy
-    from ulab import scipy
+    from ulab import numpy as np
+    from ulab import scipy as sp
 
 except ImportError:
-    import numpy
+    import numpy as np
     import scipy.special
 
-x = numpy.array([1, 2, 3])
+#a = numpy.full((3,3), '.', dtype=numpy.char)
+
+############## numpy|ulab 2D character array #############
+
+class np2DChAr: #
+  arr         = None
+  shape       = None
+  defaultChar = None
+
+  def __init__(shape=(3,3), defaultChar = '.'):
+    self.defaultChar = defaultChar; self.shape = shape
+    dcint = ord(defaultChar)
+    self.arr = np.full(shape, dcint, dtype=np.uint8)
+    
+############## main #############
+
+np2dca = np2DChAr()
 
 ### end ###
