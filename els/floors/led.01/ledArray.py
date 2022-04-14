dde59d4ada4708ad3715abcca652427b554082bc
Brygg Ullmer <ullmer@acm.org>
2022-04-13 22:30:11 -0400

    ulab/numpy interoperability (toward embedded use)

--- els/floors/led.01/ledArray.py
+++ els/floors/led.01/ledArray.py
@@ -2,5 +2,14 @@
 # By Brygg Ullmer and Sida Dai, Clemson University
 # Begun 2022-04-13
 
+try: #https://github.com/v923z/micropython-ulab
+    from ulab import numpy
+    from ulab import scipy
+
+except ImportError:
+    import numpy
+    import scipy.special
+
+x = numpy.array([1, 2, 3])
 
 ### end ###
