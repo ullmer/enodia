94c4ed5da2d527b1dda6cce9e70f0b3ede52c071
Brygg Ullmer <bullmer@clemson.edu>
2022-05-18 15:23:51 -0400

    cells

--- src/other/rr.01/bg1.py
+++ src/other/rr.01/bg1.py
@@ -8,9 +8,16 @@ bg1 = yaml.safe_load(f)
 cells = bg1["cells"]
 colors = cells.keys()
 
+tracking = {}
+
 for color in colors:
   vals    = cells[color]
   numvals = len(vals)
   print(color, numvals)
+  for val in vals:
+     tracking[val] = color
+
+for i in range(150):
+  if i not in tracking: print(i)
 
 ### end ###
