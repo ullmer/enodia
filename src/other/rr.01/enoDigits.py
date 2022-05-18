6deb9e6c03441d95c5912a5f72d0700e12f5276e
Brygg Ullmer <bullmer@clemson.edu>
2022-05-18 09:06:27 -0400

    progress

--- src/other/rr.01/enoDigits.py
+++ src/other/rr.01/enoDigits.py
@@ -3,8 +3,10 @@
 # Begun 2022-05-18
 
 class enoDigits:
-  directory   = 'num-lob/'
-  digitActors = None
+  directory       = 'num-lob/'
+  digitActors     = None #individual digits: 0, 1, ...
+  digitWidths     = None
+  numberPositions = None #potentially multi-digit numbers
  
   x, y  = 100, 100
 
@@ -16,11 +18,22 @@ class enoDigits:
   ##################### load digits #####################
 
   def loadDigits(self):
-    self.digitActors = {}
+    self.digitActors     = {}
+    self.digitWidths     = {}
+    self.numberPositions = {}
 
     for i in range(9):
       fn = self.directory + i
       a  = Actor(fn)
+
       self.digitActors[i] = a
+      self.digitWidths[i] = a
+
+  ##################### draw digits #####################
+
+  def drawDigits(self, digits):
+    if self.digitActors == None: print("enoDigits not initialized!"); return None
+
+    result = []
 
 ### end ###
