e10021f54cbf3177a92a03ba117bcd3991cd1f54
Brygg Ullmer <bullmer@clemson.edu>
2022-06-26 20:55:29 -0400

    stub

--- src/entangler.03/EnoSpaceTime.py
+++ src/entangler.03/EnoSpaceTime.py
@@ -2,9 +2,16 @@
 # By Brygg Ullmer and Sida Dai, Clemson University
 # Begun 2022-06-26
 
+############# Enodia Space base class #############
+
 class EnoSpace:
+  space = None
+  def __init__(self, space): self.space = space
+
+############# Enodia Time base class #############
 
 class EnoTime:
-
+  space = None
+  def __init__(self, time): self.space = time
 
 ### end ###
