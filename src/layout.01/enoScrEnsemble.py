db4b5b31c00a82f3002071115903856a003e155a
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2022-09-08 10:25:25 -0400

    further interactivity example

--- src/layout.01/enoScrEnsemble.py
+++ src/layout.01/enoScrEnsemble.py
@@ -3,6 +3,7 @@
 # Begun 2022-09-05
 
 import math
+import yaml
 from pgzero.builtins import Actor, animate, keyboard
 
 ################################################################
@@ -11,7 +12,8 @@ from pgzero.builtins import Actor, animate, keyboard
 class enoScrEnsemble:
 
   glyphFn1 = "mri-fargate-08h-overlay1b"
-  glyphFn2 = "person01f"
+  glyphFn2 = "person03f"
+  #glyphFn2 = "mri-fargate-08b2b"
   
   currentPos      = (150, 150)
   currentAngle    = 0.
@@ -21,13 +23,14 @@ class enoScrEnsemble:
   rotSelectThresh = 47
   rimInnerThresh  = 110
   rimOuterThresh  = 140
+  highlightMgr    = None
   
   lastMouseLoc   = None
-  rimTouched     = None
-  rotTouched     = None
+  elTouched      = None
 
   actor1, actor2 = None, None
   actors         = None
+  muteInput      = False
   
   ################# constructor #################
   
@@ -37,7 +40,11 @@ class enoScrEnsemble:
 
     self.buildVisuals()
 
-  ################# constructor #################
+  ################# getPos #################
+  
+  def getPos(self): return self.currentPos
+
+  ################# buildVisuals #################
   
   def buildVisuals(self): 
     self.actor1 = Actor(self.glyphFn1, pos=self.currentPos)
@@ -52,6 +59,8 @@ class enoScrEnsemble:
   ################# on keyboard down #################
   
   def on_key_down(self): 
+    if self.muteInput: return
+
     a1, a2 = self.actor1, self.actor2; dp = self.deltaPos
   
     if keyboard.lshift == True or keyboard.rshift == True:
@@ -86,21 +95,18 @@ class enoScrEnsemble:
     dist1 = math.dist(pos,  self.currentPos) 
     dist2 = math.dist(pos2, self.currentPos) 
   
-    self.rimTouched = (dist1 > self.rimInnerThresh and dist1 < self.rimOuterThresh)
-    self.rotTouched = (dist2 < self.rotSelectThresh)
-    #lbendTouched = 
+    self.elTouched = dist1 < self.rimOuterThresh
   
-    if self.rotTouched: 
-      if x > apos[0]: rotActor(a2, -self.deltaAngle)
-      else:           rotActor(a2,  self.deltaAngle)
-  
-    if self.rimTouched:
+    if self.elTouched:
       self.lastMouseLoc = pos
+      if self.highlightMgr is not None: self.highlightMgr.highlightByHandle(self, 0.1)
   
   ################# mouse up #################
   
   def on_mouse_move(self, pos):
-    if self.rimTouched:
+    if self.muteInput: return
+
+    if self.elTouched:
       a1 = self.actor1
       dx = pos[0] - self.lastMouseLoc[0]
       dy = pos[1] - self.lastMouseLoc[1]
@@ -113,7 +119,137 @@ class enoScrEnsemble:
   ################# mouse up #################
   
   def on_mouse_up(self):
+    if self.muteInput: return
+
     self.lastMouseLoc = None
-    self.rimTouched   = False
+    self.elTouched   = False
+
+######################################################################
+################# Enodia scrEnsemble Highlight Class #################
+
+class enoSEhighlight:
+  glyphFn1   = "mri-fargate-09a-highlight2"
+  actor      = None
+  currentPos = (150, 150)
+  currentLetterVal = 97 #ASCII for 'a'
+  currentHighlight = None
+  lastMouseLoc     = None
+  deltaPos         = 150 #delta position
+
+  hlDictByKey    = None # Dictionary of highlightable entities, single-letter key to object handle
+  hlDictByHandle = None # Dictionary of highlightable entities, object handle to single-letter key
   
+  ################# constructor #################
+  
+  def __init__(self, **kwargs):
+    self.__dict__.update(kwargs) #allow class fields to be passed in constructor
+    self.buildVisuals()
+    self.hlDictByKey    = {}
+    self.hlDictByHandle = {}
+
+  ################# buildVisuals #################
+  
+  def buildVisuals(self): 
+    self.actor = Actor(self.glyphFn1, pos=self.currentPos)
+
+  ################# register highlightable element #################
+  
+  def registerHl(self, element, muteInput = False):
+
+    keybind = chr(self.currentLetterVal)
+
+    self.hlDictByKey[keybind]    = element
+    self.hlDictByHandle[element] = keybind
+
+    if muteInput: element.muteInput = True
+    element.highlightMgr = self
+
+    self.currentLetterVal += 1
+    return keybind
+
+  ################# draw #################
+  
+  def draw(self): 
+    self.actor.draw()
+  
+  ################# on keyboard down #################
+  
+  def on_key_down(self): 
+    keys = self.hlDictByKey.keys()
+    for key in keys:
+      if keyboard.__getattr__(key): self.highlightByKey(key)
+
+    a1 = self.actor; dp = self.deltaPos
+  
+    if keyboard.lshift == False and keyboard.rshift == False:
+      if keyboard.left:  self.shiftActors([a1], -dp,   0)
+      if keyboard.right: self.shiftActors([a1],  dp,   0)
+      if keyboard.up:    self.shiftActors([a1],   0, -dp)
+      if keyboard.down:  self.shiftActors([a1],   0,  dp)
+
+  ################# shift actor #################
+  
+  def shiftActors(self, whichActors, dx, dy):
+    x, y = self.currentPos[0] + dx, self.currentPos[1] + dy; newPos = (x,y)
+    self.currentPos = newPos
+  
+    animate(self.actor, pos=newPos, tween='accel_decel', duration=.3)
+
+  ################# highlightByHandle #################
+  
+  def highlightByHandle(self, handle, duration=.3):
+    if handle not in self.hlDictByHandle:
+      print("enoSEhighlight highlightByHandle: handle not found"); return
+    key = self.hlDictByHandle[handle]
+    self.highlightByKey(key, duration)
+
+  ################# highlight #################
+  
+  def highlightByKey(self, whichKey, animDuration=.3):
+    if self.currentHighlight is not None:
+      chl = self.hlDictByKey[self.currentHighlight]
+      chl.muteInput = True
+
+    self.currentHighlight = whichKey
+    whichElement          = self.hlDictByKey[whichKey]
+    highlightNewPos       = whichElement.getPos()
+    animate(self.actor, pos=highlightNewPos, tween='accel_decel', duration=animDuration)
+    whichElement.muteInput = False
+
+  def on_mouse_down(self, pos): self.lastMouseLoc = pos
+  def on_mouse_up(self):        self.lastMouseLoc = None
+  def on_mouse_move(self, pos): 
+    if self.lastMouseLoc == None: return
+    a1 = self.actor
+    dx = pos[0] - self.lastMouseLoc[0]
+    dy = pos[1] - self.lastMouseLoc[1]
+    x, y = a1.pos[0] + dx, a1.pos[1] + dy; newPos = (x, y)
+    self.actor.pos    = newPos
+    self.currentPos   = newPos
+    self.lastMouseLoc = pos
+
+
+######################################################################
+################# Enodia scrEnsemble Highlight Class #################
+
+class enoSceneManager: 
+  yamlD = None #yaml data
+
+  ################# constructor #################
+  
+  def __init__(self, yamlFn, **kwargs):
+    self.__dict__.update(kwargs) #allow class fields to be passed in constructor
+    self.parseYamlFn(yamlFn)
+
+  ################# parse yaml filename #################
+  
+  def parseYamlFn(self, yamlFn):
+    yf = open(yamlFn, 'rt')
+    self.yamlD = yaml.safe_load(yf)
+    print(self.yamlD)
+
+    for row in self.yamlD: 
+      class enoScrEnsemble:
+      
+
 ### end ###
